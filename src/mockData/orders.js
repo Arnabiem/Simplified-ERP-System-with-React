d4be6826217e7b94c8b3f911d5cc14b74918c8c0
Arnab Sadhukhan <82289240+Arnabiem@users.noreply.github.com>
2024-03-15 21:21:14 +0530

    Add files via upload

---
+++ src/mockData/orders.js
@@ -0,0 +1,39 @@
+const mockOrders = [
+    {
+        id: 1,
+        orderId: '1',
+        customerName: 'Arnab',
+        orderDate: '2024-03-01',
+        status: 'Shipped'
+    },
+    {
+        id: 2,
+        orderId: '2',
+        customerName: 'Arun',
+        orderDate: '2024-03-10',
+        status: 'Pending'
+    },
+    {
+        id: 3,
+        orderId: '3',
+        customerName: 'Abhishek',
+        orderDate: '2024-03-04',
+        status: 'Delivered'
+    },
+    {
+        id: 4,
+        orderId: '4',
+        customerName: 'Saptak',
+        orderDate: '2024-03-05',
+        status: 'Pending'
+    },
+    {
+        id: 5,
+        orderId: '5',
+        customerName: 'Rahul',
+        orderDate: '2024-02-28',
+        status: 'Pending'
+    },    
+];
+
+export default mockOrders;
---
+++ src/mockData/products.js
@@ -0,0 +1,47 @@
+const mockProducts = [
+    {
+        id: 1,
+        name: 'Laptop',
+        category: 'Electronics',
+        price: '₹50000',
+        stock: 1
+    },
+    {
+        id: 2,
+        name: 'Crocs',
+        category: 'Footwear',
+        price: '₹1000',
+        stock: 2
+    },
+    {
+        id: 3,
+        name: 'Head Phones',
+        category: 'Gadgets',
+        price: '₹2500',
+        stock: 3
+    },
+    {
+        id: 4,
+        name: 'Camera',
+        category: 'Electronics',
+        price: '₹9000',
+        stock: 5
+    },
+    {
+        id: 5,
+        name: 'T-Shirt',
+        category: 'Fashion',
+        price: '₹1500',
+        stock: 10
+    },
+    {
+        id: 6,
+        name: 'AC',
+        category: 'Electronics',
+        price: '₹45000',
+        stock: 4
+    },
+    // Add more products as needed
+];
+
+export default mockProducts;
